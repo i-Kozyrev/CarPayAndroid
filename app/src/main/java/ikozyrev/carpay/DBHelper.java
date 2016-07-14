f6c711b6d640d898c41f18b5bae7dee79e7d002f
i-Kozyrev <Lyudjik@gmail.com>
2016-07-14 12:06:53 +0300

    test commit

--- app/src/main/java/ikozyrev/carpay/DBHelper.java
+++ app/src/main/java/ikozyrev/carpay/DBHelper.java
@@ -11,7 +11,7 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DBHelper extends SQLiteOpenHelper {
 
-    //lol
+
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "carPay";
     public static final String TABLE_DATES = "dates";
